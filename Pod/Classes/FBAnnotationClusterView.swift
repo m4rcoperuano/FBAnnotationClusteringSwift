f3b30799686230882e77f772bc26f0937af8e7c8
Marco Ledesma <marcoledesma@Marcos-MacBook-Pro.local>
2016-09-28 23:47:29 -0400

    Swift 2.3 support

--- Pod/Classes/FBAnnotationClusterView.swift
+++ Pod/Classes/FBAnnotationClusterView.swift
@@ -71,12 +71,7 @@ public class FBAnnotationClusterView : MKAnnotationView {
         setupLabel()
         setTheCount(count)
     }
-    
-    required override public init(frame: CGRect) {
-        super.init(frame: frame)
-        
-    }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
